11002b5b5dcf079a21a8ca57d6a93a75a25a9ade
Samuel Schlesinger <samuelschlesinger@Samuels-MacBook-Pro.local>
2025-04-12 14:16:42 -0400

    cleaned up the fiat shamir transform

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use curve25519_dalek::{RistrettoPoint, Scalar};
-use rand_core::{SeedableRng, CryptoRngCore};
-use rand_chacha::ChaCha20Rng;
 use group::Group;
+use rand_chacha::ChaCha20Rng;
+use rand_core::{CryptoRngCore, SeedableRng};
 use serde::{Deserialize, Serialize};
 
 use std::ops::Neg;
@@ -14,17 +14,35 @@ impl Transcript {
     fn new(label: &[u8]) -> Self {
         let mut hasher = blake3::Hasher::new();
         hasher.update(label);
-        Transcript {
-            hasher,
-        }
+        Transcript { hasher }
+    }
+
+    fn with(label: &[u8], f: impl FnOnce(&mut Transcript)) -> Scalar {
+        let mut transcript = Transcript::new(label);
+        f(&mut transcript);
+        transcript.challenge()
     }
 
     fn add_element(&mut self, element: &RistrettoPoint) {
-        self.hasher.update(&bincode::serde::encode_to_vec(element, bincode::config::standard()).unwrap());
+        self.hasher
+            .update(&bincode::serde::encode_to_vec(element, bincode::config::standard()).unwrap());
+    }
+
+    fn add_elements<'a>(&mut self, elements: impl Iterator<Item = &'a RistrettoPoint>) {
+        for element in elements {
+            self.add_element(element);
+        }
     }
 
     fn add_scalar(&mut self, scalar: &Scalar) {
-        self.hasher.update(&bincode::serde::encode_to_vec(scalar, bincode::config::standard()).unwrap());
+        self.hasher
+            .update(&bincode::serde::encode_to_vec(scalar, bincode::config::standard()).unwrap());
+    }
+
+    fn add_scalars<'a>(&mut self, scalars: impl Iterator<Item = &'a Scalar>) {
+        for scalar in scalars {
+            self.add_scalar(scalar);
+        }
     }
 
     fn rng(self) -> ChaCha20Rng {
@@ -48,10 +66,7 @@ impl PrivateKey {
         let public = PublicKey {
             w: RistrettoPoint::generator() * x,
         };
-        PrivateKey {
-            x,
-            public,
-        }
+        PrivateKey { x, public }
     }
 
     pub fn public(&self) -> &PublicKey {
@@ -121,51 +136,50 @@ impl PreIssuance {
 
     pub fn request(&self, mut rng: impl CryptoRngCore) -> IssuanceRequest {
         let params = Params::default();
-        let mut transcript = Transcript::new(b"request");
+
         let kr = params.h0 * self.r + params.h1 * self.k;
-        transcript.add_element(&kr);
         let r_t = Scalar::random(&mut rng);
         let k_t = Scalar::random(&mut rng);
         let kr_t = params.h0 * r_t + params.h1 * k_t;
-        transcript.add_element(&kr_t);
 
-        let c = transcript.challenge();
+        let c = Transcript::with(b"request", |transcript| {
+            transcript.add_elements([&kr, &kr_t].into_iter());
+        });
 
         let r_z = r_t + self.r * c;
         let k_z = k_t + self.k * c;
 
-        IssuanceRequest {
-            kr,
-            c,
-            r_z,
-            k_z,
-        }
+        IssuanceRequest { kr, c, r_z, k_z }
     }
 
-    pub fn credential(&self, public: &PublicKey, request: &IssuanceRequest, response: &IssuanceResponse) -> Option<Credential> {
+    pub fn credential(
+        &self,
+        public: &PublicKey,
+        request: &IssuanceRequest,
+        response: &IssuanceResponse,
+    ) -> Option<Credential> {
         let params = Params::default();
-        let mut transcript = Transcript::new(b"respond");
-        transcript.add_scalar(&response.e);
-        transcript.add_element(&response.a);
 
         let x_a = RistrettoPoint::generator() + request.kr + params.h2 * response.n;
-        transcript.add_element(&x_a);
         let x_g = RistrettoPoint::generator() * response.e + public.w;
-        transcript.add_element(&x_g);
+        let y_a = response.a * response.z + x_a * response.c.neg();
+        let y_g = RistrettoPoint::generator() * response.z + x_g * response.c.neg();
 
-        let y_a_prime = response.a * response.z + x_a * response.c.neg();
-        transcript.add_element(&y_a_prime);
-        let y_g_prime = RistrettoPoint::generator() * response.z + x_g * response.c.neg();
-        transcript.add_element(&y_g_prime);
-
-        let c = transcript.challenge();
+        let c = Transcript::with(b"respond", |transcript| {
+            transcript.add_scalar(&response.e);
+            transcript.add_elements([&response.a, &x_a, &x_g, &y_a, &y_g].into_iter());
+        });
 
         if c != response.c {
             return None;
         }
 
         Some(Credential {
-            a: response.a, e: response.e, r: self.r, k: self.k, n: response.n,
+            a: response.a,
+            e: response.e,
+            r: self.r,
+            k: self.k,
+            n: response.n,
         })
     }
 }
@@ -180,58 +194,57 @@ pub struct IssuanceResponse {
 }
 
 impl PrivateKey {
-    pub fn respond(&self, request: &IssuanceRequest, n: Scalar, mut rng: impl CryptoRngCore) -> Option<IssuanceResponse> {
+    pub fn respond(
+        &self,
+        request: &IssuanceRequest,
+        n: Scalar,
+        mut rng: impl CryptoRngCore,
+    ) -> Option<IssuanceResponse> {
         let params = Params::default();
-        let mut transcript = Transcript::new(b"request");
-        transcript.add_element(&request.kr);
         let kr_t = (params.h0 * request.r_z + params.h1 * request.k_z) - request.kr * request.c;
-        transcript.add_element(&kr_t);
-        let c = transcript.challenge();
+
+        let c = Transcript::with(b"request", |transcript| {
+            transcript.add_elements([&request.kr, &kr_t].into_iter());
+        });
 
         if c != request.c {
             return None;
         }
 
-        let mut transcript = Transcript::new(b"respond");
-
         let e = Scalar::random(&mut rng);
-        transcript.add_scalar(&e);
-
         let a = (RistrettoPoint::generator() + request.kr + params.h2 * n) * (e + self.x).invert();
-        transcript.add_element(&a);
-
         let x_a = RistrettoPoint::generator() + request.kr + params.h2 * n;
-        transcript.add_element(&x_a);
         let x_g = RistrettoPoint::generator() * e + self.public.w;
-        transcript.add_element(&x_g);
-
         let alpha = Scalar::random(&mut rng);
-
         let y_a = a * alpha;
-        transcript.add_element(&y_a);
         let y_g = RistrettoPoint::generator() * alpha;
-        transcript.add_element(&y_g);
 
-        let c = transcript.challenge();
+        let c = Transcript::with(b"respond", |transcript| {
+            transcript.add_scalar(&e);
+            transcript.add_elements([&a, &x_a, &x_g, &y_a, &y_g].into_iter());
+        });
 
         let z = c * (self.x + e) + alpha;
 
-        Some(IssuanceResponse {
-            a,
-            e,
-            c,
-            z,
-            n,
-        })
+        Some(IssuanceResponse { a, e, c, z, n })
     }
 }
 
-#[test]
-fn create_credential() {
-    use rand_core::OsRng;
-    let private_key = PrivateKey::random(OsRng);
-    let pre_issuance = PreIssuance::random(OsRng);
-    let req = pre_issuance.request(OsRng);
-    let resp = private_key.respond(&req, Scalar::from(20u64), OsRng).unwrap();
-    let cred = pre_issuance.credential(private_key.public(), &req, &resp).unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_credential() {
+        use rand_core::OsRng;
+        let private_key = PrivateKey::random(OsRng);
+        let pre_issuance = PreIssuance::random(OsRng);
+        let req = pre_issuance.request(OsRng);
+        let resp = private_key
+            .respond(&req, Scalar::from(20u64), OsRng)
+            .unwrap();
+        let cred = pre_issuance
+            .credential(private_key.public(), &req, &resp)
+            .unwrap();
+    }
 }
